276aca6465cd5317c9fcab2a5cfb5959f3f52fd4
Valentino Osorio Schwarz <111870582+LiveLongFlame@users.noreply.github.com>
2025-01-21 11:30:18 +0000

    Found the amount spent and spertated each amount spent according to the diffent catogorises and printed all information to the console

--- file_reading.js
+++ file_reading.js
@@ -30,15 +30,49 @@ document.addEventListener('DOMContentLoaded', function() {
                     //console.log(text); 
 
                     // Processing the information and caluating the total amount of money
-                    console.log("this is the paid amount");
-                    let total = 0;
-                    for (let i = 1; i < rows.length; i++) {
-                      if (!isNaN(Number(rows[i][2]))) {
-                        total += Number(rows[i][2]);
+                    let spent= 0;
+                    let withdrawals = 0;
+                    let food = 0;
+                    let trasport = 0;
+                    let bills = 0;
+                    let home = 0;
+                    let personal = 0;
+                    
+                    for (let i = 0; i < rows.length; i++) {
+                      if (!isNaN(Number(rows[i][2])) && rows[i][5] != "Withdrawals & Transfers") {
+                        spent += Number(rows[i][2]);
+                      }else if(!isNaN(Number(rows[i][2])) && rows[i][5] == "Withdrawals & Transfers"){
+                        withdrawals += Number(rows[i][2]);
                       }
+
+                      if(!isNaN(Number(rows[i][2])) && rows[i][5] == "Food & Beverage"){
+                        food += Number(rows[i][2]);
+                      }
+
+                      if(!isNaN(Number(rows[i][2])) && rows[i][5] == "Transport & Travel"){
+                        trasport += Number(rows[i][2]);
+                      }
+
+                      if(!isNaN(Number(rows[i][2])) && rows[i][5] == "Retail & Personal"){
+                        personal += Number(rows[i][2]);
+                      }
+
+                      if(!isNaN(Number(rows[i][2])) && rows[i][5] == "Bills & Payments"){
+                        bills += Number(rows[i][2]);
+                      }
+                      if(!isNaN(Number(rows[i][2])) && rows[i][5] == "Home & Property"){
+                        home += Number(rows[i][2]);
+                      }
+
                     }
 
-                      console.log("the total amount spent  is: $" , (total.toFixed(2) * -1))
+                      console.log("Spent Amount: $" , (spent.toFixed(2) * -1))
+                      console.log("With Drawn Amount: $" , (withdrawals.toFixed(2) * -1))
+                      console.log("Food & Beverage Amount: $" , (food.toFixed(2) * -1))
+                      console.log("Transport & Travel Amount: $" , (trasport.toFixed(2) * -1))
+                      console.log("Retail & Personal Amount: $" , (personal.toFixed(2) * -1))
+                      console.log("Bills & Payments Amount: $" , (bills.toFixed(2) * -1))
+                      console.log("Home & Property Amount: $" , (home.toFixed(2) * -1))
                 };
                 reader.readAsText(file);
         } else {
