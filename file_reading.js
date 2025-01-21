ca79c4b97266391630589f46c8f3d2b9d822dd61
Valentino Osorio Schwarz <111870582+LiveLongFlame@users.noreply.github.com>
2025-01-21 03:31:18 +0000

    add js and printed out the csv contents on to the console. However problem found where cannot add js code to spertate file cause of a weird error need to discover more about

---
+++ file_reading.js
@@ -0,0 +1,6 @@
+//!Broken need to add to file
+const form = document.getElementById("form")
+const file = document.getElementById("file")
+
+console.log("Hello this is form the js file")
+//todo: Added file handling so that no empty files can be uip
